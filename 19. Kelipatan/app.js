7a54a90c9f605d6983317352f90683b236a1165b
idiotRun <doflamingod642@gmail.com>
2023-11-01 23:58:43 +0800

    refactor kelipatan

--- 19. Kelipatan/app.js
+++ 19. Kelipatan/app.js
@@ -1,43 +1,17 @@
-function generateMultiplesOfTwo(limit) {
-   const multiplesOfTwo = [];
-   for (let i = 2; i <= limit; i += 2) {
-      multiplesOfTwo.push(i);
+function generateMultiplesOfN(limit, n) {
+   const multiples = [];
+   for (let i = n; i <= limit; i += n) {
+      multiples.push(i);
    }
-   return multiplesOfTwo;
+   return multiples;
 }
 
-const multiplesOfTwo = generateMultiplesOfTwo(100);
+const multiplesOfTwo = generateMultiplesOfN(100, 2);
+const multiplesOfThree = generateMultiplesOfN(100, 3);
+const multiplesOfFour = generateMultiplesOfN(100, 4);
+const multiplesOfFive = generateMultiplesOfN(100, 5);
+
 console.log(multiplesOfTwo);
-
-function generateMultiplesOfThree(limit) {
-   const multiplesOfThree = [];
-   for (let i = 3; i <= limit; i += 3) {
-      multiplesOfThree.push(i);
-   }
-   return multiplesOfThree;
-}
-
-const multiplesOfThree = generateMultiplesOfThree(100);
 console.log(multiplesOfThree);
-
-function generateMultiplesOfFour(limit) {
-   const multiplesOfFour = [];
-   for (let i = 4; i <= limit; i += 4) {
-      multiplesOfFour.push(i);
-   }
-   return multiplesOfFour;
-}
-
-const multiplesOfFour = generateMultiplesOfFour(100);
 console.log(multiplesOfFour);
-
-function generateMultiplesOfFive(limit) {
-   const multiplesOfFive = [];
-   for (let i = 5; i <= limit; i += 5) {
-      multiplesOfFive.push(i);
-   }
-   return multiplesOfFive;
-}
-
-const multiplesOfFive = generateMultiplesOfFive(100);
 console.log(multiplesOfFive);
