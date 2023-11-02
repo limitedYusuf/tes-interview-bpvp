2a0871e666534bac53f3b81c9d409509603c0f0e
idiotRun <doflamingod642@gmail.com>
2023-11-02 19:51:49 +0800

    push binary

---
+++ 49. Binary/app.js
@@ -0,0 +1,27 @@
+function binarySearch(arr, target) {
+   let left = 0;
+   let right = arr.length - 1;
+
+   while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+      if (arr[mid] === target) {
+         return mid;
+      } else if (arr[mid] < target) {
+         left = mid + 1;
+      } else {
+         right = mid - 1;
+      }
+   }
+
+   return -1;
+}
+
+const sortedArray = [5, 7, 10, 15, 20];
+const targetValue = 10;
+const resultIndex = binarySearch(sortedArray, targetValue);
+
+if (resultIndex !== -1) {
+   console.log(`Elemen ${targetValue} ditemukan di indeks ${resultIndex}.`);
+} else {
+   console.log(`Elemen ${targetValue} tidak ditemukan.`);
+}
