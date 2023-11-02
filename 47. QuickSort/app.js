b5cbb36570b4e3924c0233565c52bb14300fc978
idiotRun <doflamingod642@gmail.com>
2023-11-02 19:49:15 +0800

    push quick

---
+++ 47. QuickSort/app.js
@@ -0,0 +1,23 @@
+function quickSort(arr) {
+   if (arr.length <= 1) {
+      return arr;
+   }
+
+   const pivot = arr[0];
+   const left = [];
+   const right = [];
+
+   for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < pivot) {
+         left.push(arr[i]);
+      } else {
+         right.push(arr[i]);
+      }
+   }
+
+   return quickSort(left).concat(pivot, quickSort(right));
+}
+
+const unsortedArray = [64, 34, 25, 12, 22, 11, 90];
+const sortedArray = quickSort(unsortedArray);
+console.log("Array yang sudah diurutkan:", sortedArray);
